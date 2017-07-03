680d8b470f2641df0a2c7f4a22804c6cc28f1bfd
tuiche000 <845042291@qq.com>
2017-07-04 06:46:57 +0800

    面向对象思想重写分页

--- case/paging.php
+++ case/paging.php
@@ -7,6 +7,7 @@
  */
 
 include "user/public/mysql_config.php";
+include "user/class/paging.class.php";
 
 $sql = "SELECT COUNT(*) FROM user1";
 
@@ -14,32 +15,18 @@ $result = mysqli_query($conn,$sql);
 
 $row = mysqli_fetch_row($result);
 
-$total = $row[0];
+$page = new paging($row[0],2);
 
-//每页多少
-$lenght = 2;
-
-//总页数
-$pagenum = ceil($total/$lenght);
-
-//计算当前页
-$page = $_GET['p'] ? $_GET['p']:1;
-
-//计算offset
-$offset = ($page-1) * $lenght;
 
 //输出每页数据
-$sqlUser = "select * from user1 order by id limit {$offset},{$lenght}";
+$sqlUser = "select * from user1 order by id {$page->limit}";
 $resultUser = mysqli_query($conn,$sqlUser);
 while ($rowUser = mysqli_fetch_row($resultUser)){
     print_r($rowUser[1]);
     echo "<br>";
 }
+$page->show();
 
-//计算上一页下一页
-$prevpage = $page-1<1 ? 1 : $page-1;
-$nextpage = $page+1>$pagenum ? $pagenum : $page+1;
-echo "<a href='paging.php?p=$prevpage'>上一页</a>|<a href='paging.php?p=$nextpage'>下一页</a>";
 
 
 
--- case/user/class/paging.class.php
+++ case/user/class/paging.class.php
@@ -5,3 +5,49 @@
  * Date: 2017/7/2
  * Time: 20:46
  */
+
+//分页面向对象分装
+class paging {
+    //成员变量
+    var $total;
+    var $length;
+    var $pagenum;
+    var $page;
+    var $offset;
+    var $prevpage;
+    var $nextpage;
+
+    //构造函数 ，是一种特殊的方法。主要用来在创建对象时初始化对象， 即为对象成员变量赋初始值，总与new运算符一起使用在创建对象的语句中。
+    function __construct($total,$length){
+        $this->total = $total;
+        $this->length = $length;
+        $this->pagenum = ceil($this->total/$this->length);
+        $this->page = $_GET['p'] ? $_GET['p']:1;
+        $this->offset = ($this->page-1) * $this->length;
+        $this->limit = "limit {$this->offset},{$this->length}";
+        $this->prevpage();
+        $this->nextpage();
+    }
+
+    //成员函数
+    function prevpage(){
+        if($this->page-1<0){
+            $this->prevpage = 0;
+        }else{
+            $this->prevpage = $this->page-1;
+        }
+    }
+
+    function nextpage(){
+        if($this->page+1 > $this->total){
+            $this->nextpage = $this->pagenum;
+        }else{
+            $this->nextpage = $this->page+1;
+        }
+    }
+
+    function show(){
+        echo "<a href='paging.php?p={$this->prevpage}'>上一页</a>|<a href='paging.php?p={$this->nextpage}'>下一页</a>";
+    }
+
+}
